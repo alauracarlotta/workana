61ef2c034d737e2246a4c5bee4b82cf839f1fe26
alauracarlotta <carlotta.custodio@gmail.com>
2024-10-10 15:57:00 -0300

    style: add config to return static and media directories

--- rent_control/urls.py
+++ rent_control/urls.py
@@ -16,9 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-import apps.core.views
+from django.conf import settings
+from django.conf.urls.static import static
+import apps.core.views 
+
 
 urlpatterns = [
-    path('', apps.core.views.home),
+    path('', apps.core.views.list_location, name='list-location'),
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
