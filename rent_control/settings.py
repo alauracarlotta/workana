cdd53bf60066ae4f4deb00e8edc44c0375134494
alauracarlotta <carlotta.custodio@gmail.com>
2024-10-25 12:46:21 -0300

    feat: add navbar and message files in base template

--- rent_control/settings.py
+++ rent_control/settings.py
@@ -128,3 +128,14 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# -------- MESSAGES -------- #
+from django.contrib.messages import constants
+
+MESSAGE_TAGS = {
+    constants.ERROR: 'alert-danger',
+    constants.WARNING: 'alert-warninig',
+    constants.DEBUG: 'alert-danger',
+    constants.SUCCESS: 'alert-success',
+    constants.INFO: 'alert-info',
+}
